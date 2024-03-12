f4b002f3aa6fea34598eaa8587cf7d0ae0558666
shurupov <bolteg86@ya.ru>
2024-03-13 02:43:28 +0400

    Game Engine Displaying Moving and Rotating Model Customization

--- src/main/java/org/shurupov/spaceflight/engine/GameLoop.java
+++ src/main/java/org/shurupov/spaceflight/engine/GameLoop.java
@@ -22,12 +22,24 @@ public class GameLoop {
     Renderer renderer = new Renderer(); // визуализатор моделей
     StaticShader shader = new StaticShader(); // шейдер статических моделей
 
-    float[] vertices = {
-        -0.5f,  0.5f, 0f, // V0
-        -0.5f, -0.5f, 0f, // V1
-        0.5f, -0.5f, 0f, // V2
-        0.5f,  0.5f, 0f, // V3
+    int modelWidth = 235;
+    int modelHeight = 630;
+    int maxDimension = Math.max(modelWidth, modelHeight);
+    int dimensionDifference = Math.abs(modelHeight-modelWidth);
+    int dimensionSum = modelHeight+modelWidth;
+    float[] modelVerticesInModelUnit = {
+        0 + (float) dimensionDifference / 2 / maxDimension - 0.5f, 0.5f, 0f,
+        0 + (float) dimensionDifference / 2 / maxDimension - 0.5f, -0.5f, 0f,
+        0 + (float) dimensionSum / 2 / maxDimension - 0.5f, -0.5f, 0f,
+        0 + (float) dimensionSum / 2 / maxDimension - 0.5f, 0.5f, 0f,
     };
+
+    float[] vertices = new float[modelVerticesInModelUnit.length];
+    for (int i = 0; i < vertices.length; i += 3) {
+      vertices[i] = modelVerticesInModelUnit[i];
+      vertices[i+1] = modelVerticesInModelUnit[i+1];
+      vertices[i+2] = 0f;
+    }
 
     int[] indices = {
         0, 1, 3, // Верхний левый треугольник
@@ -44,21 +56,30 @@ public class GameLoop {
     // загружаем массив вершин, текстурных координат и индексов в память GPU
     RawModel model = loader.loadToVao(vertices, textureCoords, indices);
     // загрузим текстуру используя загрузчик
-    ModelTexture texture = new ModelTexture(loader.loadTexture("src/main/resources/images/image.png"));
+    ModelTexture texture = new ModelTexture(loader.loadTexture("src/main/resources/images/spaceRockets_003.png"));
     // Создание текстурной модели
     TexturedModel staticModel = new TexturedModel(model, texture);
 
-    Entity entity = new Entity(staticModel,
-        new Vector3f(0f, 0, 0),
-        0, 0, 30,
-        1.0f);
+    Entity entity = new Entity(
+        staticModel,
+        new Vector3f(-0.02f, 0.3f, 0),
+        0, 0, -90,
+        0.8f
+    );
 
     // запускаем цикл пока пользователь не закроет окно
     while (DisplayManager.shouldDisplayClose()) {
-      if(Keyboard.isKeyDown(GLFW.GLFW_KEY_A) || Keyboard.isKeyDown(GLFW.GLFW_KEY_LEFT))
-        entity.increasePosition(-0.1f, 0, 0);
-      else if(Keyboard.isKeyDown(GLFW.GLFW_KEY_D))
-        entity.increasePosition(0.1f, 0, 0);
+
+      //TODO Перетащить в отдельную команду/команды ловли событий с устройств ввода
+      if(Keyboard.isKeyDown(GLFW.GLFW_KEY_A) || Keyboard.isKeyDown(GLFW.GLFW_KEY_LEFT)) {
+        entity.increasePosition(-0.01f, 0, 0);
+      } else if(Keyboard.isKeyDown(GLFW.GLFW_KEY_D) || Keyboard.isKeyDown(GLFW.GLFW_KEY_RIGHT)) {
+        entity.increasePosition(0.01f, 0, 0);
+      } else if(Keyboard.isKeyDown(GLFW.GLFW_KEY_W) || Keyboard.isKeyDown(GLFW.GLFW_KEY_UP)) {
+        entity.increasePosition(0f, 0.01f, 0);
+      } else if(Keyboard.isKeyDown(GLFW.GLFW_KEY_S) || Keyboard.isKeyDown(GLFW.GLFW_KEY_DOWN)) {
+        entity.increasePosition(0f, -0.01f, 0);
+      }
       if (Mouse.isButtonDown(GLFW.GLFW_MOUSE_BUTTON_LEFT))
         entity.increaseRotation(0, 0, 1);
       else if (Mouse.isButtonDown(GLFW.GLFW_MOUSE_BUTTON_RIGHT))
