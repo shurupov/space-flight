9d369490bb570c3fe3a1ff2991b5694c0c7cdf10
shurupov <bolteg86@ya.ru>
2024-03-24 23:30:56 +0400

    Removed IoC

--- src/main/java/org/shurupov/spaceflight/engine/ioc/context/ContextProcessor.java
+++
@@ -1,7 +0,0 @@
-package org.shurupov.spaceflight.engine.ioc.context;
-
-import java.util.Map;
-
-public interface ContextProcessor {
-  void process(Map<String, Object> context, Object[] args);
-}
--- src/main/java/org/shurupov/spaceflight/engine/ioc/strategy/IoCStrategy.java
+++
@@ -1,5 +0,0 @@
-package org.shurupov.spaceflight.engine.ioc.strategy;
-
-public interface IoCStrategy {
-  <T> T resolve(String key, Object ...args);
-}
